0ba3e6629d2a5690b2ae6653bc7e6c0590ddb8ff
Damon Jenkins <damonsfjenkins@gmail.com>
2020-03-15 21:13:09 +1300

    GreyBoxed Teleport Reticle

--- Assets/Scripts/PaintPot.cs
+++ Assets/Scripts/PaintPot.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class PaintPot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject reticle;
+    RaycastHit hitVar;
+
+    private void Start()
+    {
+        reticle = GameObject.Find("Reticle");
+    }
+
+    private void Update()
+    {
+
+        if (GetComponent<Rigidbody>().velocity.magnitude > 2f)
+        {
+            reticle.SetActive(true);
+            reticle.transform.rotation = Quaternion.identity;
+
+            Physics.SphereCast(transform.position, 0.5f, new Vector3(0.0f, -1.0f, 0.0f), out hitVar, 2000.0f, LayerMask.GetMask("Ground"));
+            reticle.transform.position = transform.position - new Vector3(0.0f, hitVar.distance, 0.0f);
+
+        }
+        else {
+            reticle.SetActive(false);
+        }
+    }
+
     private void OnCollisionEnter(Collision _collision)
     {
         if (_collision.contacts[0].normal.y > 0.98f){
