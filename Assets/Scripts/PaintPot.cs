3c81833d1ec837f811dd1c3f56ca87cbffda2702
Damon Jenkins <damonsfjenkins@gmail.com>
2020-03-15 15:56:44 +1300

    Stick Pot To Ground

---
+++ Assets/Scripts/PaintPot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintPot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision _collision)
+    {
+        if (_collision.contacts[0].normal.y > 0.98f){
+            GetComponent<Rigidbody>().velocity = new Vector3();
+        }
+    }
+}
