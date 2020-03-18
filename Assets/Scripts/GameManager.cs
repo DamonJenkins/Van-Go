c6a7f1efe4c76022332198e2195ef52d7f1a2cc5
Damon Jenkins <dj.rockster@hotmail.com>
2020-03-18 17:13:29 +1300

    Added Timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	bool timeRunning = false;
+	bool timerVisible =  false;
+	float timerVal = 0.0f;
+	Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		timerVisible = PlayerPrefs.GetInt("showTimer", 0) == 1;
+
+		timerText = GameObject.Find("TimerText").GetComponent<Text>();
+
+		if (!timerVisible) {
+			timerText.gameObject.SetActive(false);
+		}
+
+		StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (timeRunning){
+			timerVal += Time.deltaTime;
+		}
+
+		if (timerVisible) {
+			timerText.text = string.Format("{0:00:00.00}s", timerVal).Replace(".", ":");
+		}
+    }
+
+	public void StartTimer() {
+		timeRunning = true;
+	}
+
+	public void StopTimer()
+	{
+		timeRunning = false;
+	}
+}
