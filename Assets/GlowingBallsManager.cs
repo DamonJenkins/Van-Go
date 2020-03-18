d32094390ad4ef9e8f70f01d88741be29810bf3f
Jedddie <34596339+Jedddie@users.noreply.github.com>
2020-03-18 20:59:53 +1300

    Added glowy indicator orbs to portal prefab

---
+++ Assets/GlowingBallsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingBallsManager : MonoBehaviour
+{
+
+    [SerializeField]
+    Material onMat, offMat;
+    [SerializeField]
+    List<MeshRenderer> balls;
+    [SerializeField]
+    List<Paintable> paintables;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(MeshRenderer ball in balls)
+        {
+            if(ball != null) ball.material = offMat;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < balls.Count && i < paintables.Count; i++)
+        {
+            if (paintables[i] != null && paintables[i].IsPainted() && balls[i].material != onMat) balls[i].material = onMat;
+        }
+    }
+}
